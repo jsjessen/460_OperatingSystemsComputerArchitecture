5692cdd6ff0fc580c6847ba47d13dcd9d176e04d
James Jessen <jsjessen@gmail.com>
2015-04-29 13:44:41 -0700

    Clean up git and add vdisk variants to ignore list

--- last/login.c
+++ last/login.c
@@ -1,5 +1,6 @@
-// 
-//                           Proc#2:
+// James Jessen
+// CptS 460 - Spring 2015
+
 //   (1). P2 is a login process. It executes /bin/login on the console terminal 
 //        /dev/tty0. So the special file /dev/tty0 MUST exist. To support serial 
 //        terminals and printer, /dev/ttyS0, /dev/ttyS1 and /dev/lp0 must also 
@@ -22,71 +23,56 @@
 //   (by taking on the user's uid). It chdir to user's HOMEDIR and execute the 
 //   listed program, which is normally the sh (/bin/sh).
 //  
-//   (3). then (in sh) it loops forever (until "logout" or Contro-D):
-//         {
-//            prompts for a command line, e.g. cmdLine="cat filename"
-//            if (cmd == "logout") 
-//               syscall to die;
-// 
-//            // if just ONE cmd:  
-//            pid = fork();
-//            if (pid==0)
-//                exec(cmdLine);
-//            else
-//                pid = wait(&status);
-//         }    
-// 
-//    (3). When the child proc terminates (by exit() syscall to MTX kernel), it 
-//    wakes up sh, which prompts for another cmdLine, etc.
-// 
-//    (4). When sh dies, it wakes up its parent, INIT, which forks another
-//         login process.         
 // ==========================================================================
-// 
-// //***********************************************************************
-// //                   LOGIC of login.c file
-// //***********************************************************************
-// char *tty;
-// 
-// main(int argc, char *argv[])   // invoked by exec("login /dev/ttyxx")
-// {
-//   tty =  argv[1];
-// 
-//     // JJ: close anything you may of inherited from your parent
-//  1. close(0); close(1); close(2); // login process may run on different terms
-//     // JJ: parent is init() on console
-// 
-//     // JJ: printing won't work until you open your own FD
-//  2. // open its own tty as stdin, stdout, stderr
-//     // JJ: Now we have our own 0, 1, 2 and can printf
-//     //     every process must remember which tty (terminal) it is running on
-//     //     printf shouldn't be showing up on another guys terminal!
-// 
-//  3. settty(tty);   // store tty string in PROC.tty[] for putc()
-// 
-//   // NOW we can use printf, which calls putc() to our tty
-//   printf("KCLOGIN : open %s as stdin, stdout, stderr\n", tty);
-// 
-//   signal(2,1);  // ignore Control-C interrupts so that 
-//                 // Control-C KILLs other procs on this tty but not the main sh
-// 
-//   while(1)
-//   {
-//     1. show login:           to stdout
-//     2. read user name        from stdin
-//     3. show passwd:
-//     4. read user passwd
-// 
-//     5. verify user name and passwd from /etc/passwd file
-//     // JJ: open the file in read-only mode
-//     //     tokenize the different fields
-//     //     strcmp  name with name, and pword with pword
-// 
-//     6. if (user account valid){
-//           setuid to user uid. (think he said setgid is done at the same time as setuid)
-//           chdir to user HOME directory.
-//           exec to the program in users's account
-//        }
-//        printf("Login failed, try again\n");
-//   }
-// }
+
+char *tty;
+int stdin, stdout, stderr;
+
+#include "ucode.c"
+
+// parent is init() on console
+int main(int argc, char *argv[])   // invoked by exec("login /dev/ttyxx")
+{
+    tty = argv[1];
+
+    // JJ: close anything you may of inherited from your parent
+    close(0); close(1); close(2); // login process may run on different terms
+
+    // Open own tty as stdin, stdout, stderr in order to display messages 
+    stdin = open(tty, O_RDONLY);
+    stdout = open(tty, O_WRONLY); // open("/dev/tty0", O_WRONLY)
+    stderr = open(tty, O_WRONLY); 
+
+    // JJ: Now we have our own 0, 1, 2 and can printf
+    //     every process must remember which tty (terminal) it is running on
+    //     printf shouldn't be showing up on another guys terminal!
+
+    settty(tty);  // store tty string in PROC.tty[] for putc()
+
+    // NOW we can use printf, which calls putc() to our tty
+    printf("KCLOGIN : open %s as stdin, stdout, stderr\n", tty);
+
+    signal(2,1);  // ignore Control-C interrupts so that 
+    // Control-C KILLs other procs on this tty but not the main sh
+
+    while(1)
+    {
+        // 1. show login:           to stdout
+        printf("Login: ");
+        // 2. read user name        from stdin
+        // 3. show passwd:
+        // 4. read user passwd
+
+        // 5. verify user name and passwd from /etc/passwd file
+        // JJ: open the file in read-only mode
+        //     tokenize the different fields
+        //     strcmp  name with name, and pword with pword
+
+        // 6. if (user account valid){
+        //    setuid to user uid. (think he said setgid is done at the same time as setuid)
+        //    chdir to user HOME directory.
+        //    exec to the program in users's account
+        //    }
+        printf("Login failed, try again\n");
+    }
+}
